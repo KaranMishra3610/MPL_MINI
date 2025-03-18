3408ce3df5efb9dc42e4ab0c573fe7df4ec720ae
KaranMishra3610 <145427007+KaranMishra3610@users.noreply.github.com>
2025-03-18 18:45:08 +0530

    Create constants.dart

---
+++ constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  static const String menuCollection = "canteen_menu";
+  static const String cartCollection = "user_cart";
+
+  static const Color primaryColor = Colors.orange;
+  static const Color accentColor = Colors.blue;
+}
